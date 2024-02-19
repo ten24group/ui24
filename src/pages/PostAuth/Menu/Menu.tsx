ddf2abdaeb842466519913fc1aed5ddd523dd537
Gaurav Singh <gauravsingh.re@gmail.com>
2024-02-19 19:26:58 +0530

    simplified menu component

--- src/pages/PostAuth/Menu/Menu.tsx
+++ src/pages/PostAuth/Menu/Menu.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import { Menu as AntMenu } from 'antd';
+import { Link } from '../../../forms/PostAuthForm';
 
 type MenuItem = Required<MenuProps>['items'][number];
 
@@ -22,39 +23,34 @@ function getItem(
 }
   
 const items: MenuProps['items'] = [
-getItem('Navigation One', 'sub1', <MailOutlined />, [
-    getItem('Item 1', 'g1', null, [getItem('Option 1', '1'), getItem('Option 2', '2')], 'group'),
-    getItem('Item 2', 'g2', null, [getItem('Option 3', '3'), getItem('Option 4', '4')], 'group'),
-]),
 
-getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
-    getItem('Option 5', '5'),
-    getItem('Option 6', '6'),
-    getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
+getItem('Account', 'sub2', <AppstoreOutlined />, [
+    getItem(<Link title='Account List' url = "/accountList" />, '5'),
+    getItem(<Link title='Add New Account' url = "/accountCreate" />, '6'),
 ]),
 
 { type: 'divider' },
 
-getItem('Navigation Three', 'sub4', <SettingOutlined />, [
+getItem('Navigation With Submenu', 'sub4', <SettingOutlined />, [
     getItem('Option 9', '9'),
     getItem('Option 10', '10'),
     getItem('Option 11', '11'),
     getItem('Option 12', '12'),
-]),
-
-//getItem('Group', 'grp', null, [getItem('Option 13', '13'), getItem('Option 14', '14')], 'group'),
+    getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
+])
 ];
 
 export const Menu = () => {
+    
     const onClick: MenuProps['onClick'] = (e) => {
         console.log('click ', e);
     };
 
     return <AntMenu
-    onClick={onClick}
-    style={{ width: "100%" }}
-    theme="dark"
-    mode="horizontal"
-    items={items}
+      onClick={onClick}
+      style={{ width: "100%" }}
+      theme="dark"
+      mode="horizontal"
+      items={items}
   />
 }
