62c0a9c4b84f89fea7c3a2fa2cebcd8bc23a5a80
Gaurav Singh <gauravsingh.re@gmail.com>
2024-02-26 10:24:44 +0530

    dynami properties and style attributes

--- src/core/forms/formConfig.ts
+++ src/core/forms/formConfig.ts
@@ -1,16 +1,19 @@
+import React from "react";
 import { ICreateButtons } from "./buttons/Buttons";
-import { IFormField } from "./formfields/formField";
+import { IFormField } from "./FormField/FormField";
 
 interface IFormConfig {
-    name: string;
+    name?: string;
     className?: string;
     initialValues?: any;
 }
-  
+
 interface ICustomForm extends ICreateButtons {
     formConfig?: IFormConfig;
     propertiesConfig: Array<IFormField>;
     onSubmit: (values: any) => void;
+    children?: React.ReactNode;
+    style?: React.CSSProperties;
 }
 
 
