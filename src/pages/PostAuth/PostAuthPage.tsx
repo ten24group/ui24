b95a3895dfc49b9197b17e6f569a05bacfcdad34
Gaurav Singh <gauravsingh.re@gmail.com>
2024-02-26 10:27:10 +0530

    post auth page isolated

--- src/pages/PostAuth/PostAuthPage.tsx
+++ src/pages/PostAuth/PostAuthPage.tsx
@@ -1,38 +1,28 @@
 import React from 'react';
-import { DashboardLayout } from "../../layout"
-import { IFormConfig } from '../../core/forms/formConfig';
-import { IFormField } from '../../core';
-import { Table } from '../../table/Table';
-import { DynamicForm } from '../../forms/PostAuthForm';
-import { usePageConfig } from '../../core';
-import { Link } from '../../core/common';
-import { PageHeader } from '@ant-design/pro-layout';
-import { Breadcrumb, Card } from "antd";
+import { PostAuthLayout } from "../../layout"
+//import { DynamicForm } from '../../forms/PostAuthForm';
+import { PageHeader, IPageHeader } from './PageHeader/PageHeader';
 import "./PostAuthPage.css";
+/**
+ * Dynamic Properties
+ * 
+ * @returns 
+ */
 
-export const PostAuthPage = ({ metaDataUrl } : { metaDataUrl : string }) => {
-    
-    const { propertiesConfig, pageType } = usePageConfig( metaDataUrl );
-
-    const breadcrumbs = <Breadcrumb items={[{ title: <Link title="Account List" url={"/account/list"} /> }, { title: "Add New Account"}]} />
-
-    return <>{ propertiesConfig && <DashboardLayout>
+interface IPostAuthPage extends IPageHeader{
+    metaDataUrl: string;
+    CustomPageHeader?: React.ReactNode;
+    children?: React.ReactNode;
+}
+export const PostAuthPage = ({ metaDataUrl, CustomPageHeader, pageTitle, children } : IPostAuthPage ) => {
+    return <PostAuthLayout>
         <div style={{ paddingTop: "1%"}}>
             <div className = "PostAuthContainer" >
-                <div className="PageHeader">
-                    <PageHeader
-                    className="site-page-header"
-                    title="Accounts"
-                    breadcrumb={ breadcrumbs }
-                    subTitle="" />
-                </div>
+                { CustomPageHeader ? CustomPageHeader : <PageHeader pageTitle={ pageTitle } />}
                 <div className="PageContent">
-                    <Card>
-                        { pageType==="form" && <DynamicForm formName='addForm' propertiesConfig={ propertiesConfig } />  }
-                        { pageType === "list" && <Table propertiesConfig={ propertiesConfig } /> }
-                    </Card>
+                    { children }
                 </div>
             </div>
         </div>
-    </DashboardLayout>}</>;
+    </PostAuthLayout>;
 }
