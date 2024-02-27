9ac3c8fae6f577ea0003446369249f10da13146f
Gaurav Singh <gauravsingh.re@gmail.com>
2024-02-27 16:51:21 +0530

    logic to render dynamic page content based on page type

--- src/pages/PostAuth/PostAuthPage.tsx
+++ src/pages/PostAuth/PostAuthPage.tsx
@@ -1,28 +1,46 @@
 import React from 'react';
 import { PostAuthLayout } from "../../layout"
-//import { DynamicForm } from '../../forms/PostAuthForm';
 import { PageHeader, IPageHeader } from './PageHeader/PageHeader';
+import { ICustomForm } from '../../core/forms/formConfig';
 import "./PostAuthPage.css";
-/**
- * Dynamic Properties
- * 
- * @returns 
- */
+import { Card } from 'antd';
+import { PostAuthForm } from '../../forms/PostAuthForm';
 
-interface IPostAuthPage extends IPageHeader{
+type IPageType = "list" | "form" | "accordion"
+
+interface IRenderFromPageType {
+    pageType?: IPageType;
+    cardStyle?: React.CSSProperties;
+    formPageConfig?: ICustomForm;
+}
+
+interface IPostAuthPage extends IPageHeader, IRenderFromPageType {
     metaDataUrl: string;
     CustomPageHeader?: React.ReactNode;
     children?: React.ReactNode;
 }
-export const PostAuthPage = ({ metaDataUrl, CustomPageHeader, pageTitle, children } : IPostAuthPage ) => {
+
+
+export const PostAuthPage = ({ metaDataUrl, CustomPageHeader, children, ...props } : IPostAuthPage ) => {
     return <PostAuthLayout>
         <div style={{ paddingTop: "1%"}}>
             <div className = "PostAuthContainer" >
-                { CustomPageHeader ? CustomPageHeader : <PageHeader pageTitle={ pageTitle } />}
+                { CustomPageHeader ? CustomPageHeader : <PageHeader {...props} />}
                 <div className="PageContent">
-                    { children }
+                    { children && children }
+                    { !children && <RenderFromPageType {...props} />}
                 </div>
             </div>
         </div>
     </PostAuthLayout>;
+}
+
+const RenderFromPageType = ( {pageType, cardStyle, formPageConfig}: IRenderFromPageType ) => {
+    
+    switch( pageType ) {
+        case "list": return <Card style={ cardStyle } > List Page </Card>;
+        case "form": return <Card style={ cardStyle } > <PostAuthForm {...formPageConfig} /> </Card>;
+        case "accordion": return <div> Accordion Page </div>;
+        default: return <>Invalid Page Type</>;
+    }
 }
