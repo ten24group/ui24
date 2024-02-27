6d7f650b848196ab8eaf733ca760f261e9630641
Gaurav Singh <gauravsingh.re@gmail.com>
2024-02-27 14:41:05 +0530

    added menu config and api url in core config.

--- src/core/config/config.ts
+++ src/core/config/config.ts
@@ -3,15 +3,26 @@ import { createAxiosInstance } from "../api/config"
 interface IFW24Config {
     baseURL: string;
     appLogo: string;
+    menuItems?: Array<any>;
+    menuApiUrl?: string;
 }
 
-const FW24Config = {} as IFW24Config;
+const FW24Config = {
+    menuApiUrl : ""
+} as IFW24Config;
 
 const initFW24Config = ( config : IFW24Config ) => {
 
     createAxiosInstance(config.baseURL);
 
     FW24Config.appLogo = config.appLogo;
+    
+    if( config.menuItems ){
+        FW24Config.menuItems = config.menuItems;
+    }
+    if( config.menuApiUrl ){
+        FW24Config.menuApiUrl = config.menuApiUrl;
+    }
 }
 
 export { FW24Config, initFW24Config }
