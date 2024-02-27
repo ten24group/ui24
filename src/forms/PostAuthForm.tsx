79a7d04a0c9f98ad7ea815adf25cfa019791f257
Gaurav Singh <gauravsingh.re@gmail.com>
2024-02-27 16:51:46 +0530

    call dynamic API endpoint on form submission

--- src/forms/PostAuthForm.tsx
+++ src/forms/PostAuthForm.tsx
@@ -2,24 +2,51 @@ import { Button, Checkbox, Form, Input } from 'antd';
 import React, { Component, ReactNode } from 'react';
 import { ICreateButtons, CreateButtons } from '../core/forms';
 import { IFormConfig } from '../core/forms/formConfig';
+import { useNavigate } from 'react-router-dom';
 import { IFormField } from '../core/forms';
 import { FormField } from '../core/forms';
 import { ICustomForm } from '../core/forms/formConfig';
+import { postMethod } from '../core';
 
 export function PostAuthForm({
     formConfig = { name: "customForm" },
     propertiesConfig = [],
     onSubmit,
     formButtons = [],
-    children
+    children,
+    submitApiUrl = "",
+    submitSuccessRedirect = ""
 } : ICustomForm ) {
+  const navigate = useNavigate();
+
+  const customOnSubmit = async (values: any) => {
+    
+    if( submitApiUrl !== "") {
+      const response = await postMethod(submitApiUrl, values);
+      if( response ){
+        //handle success
+        if( submitSuccessRedirect !== "") {
+          //redirect to the page
+          navigate( submitSuccessRedirect)
+        }
+      } else {
+        //1. general error
+        //2. form level error
+        //3. field level error
+        //handle failure
+      }
+    } else {
+      //call when defined
+      onSubmit && onSubmit(values)
+    }
+  }
 
     return <Form
     name={ formConfig.name || "" }
     className={ formConfig?.className || "" }
     initialValues={ formConfig?.initialValues || {} }
     layout="vertical"
-    onFinish={onSubmit}
+    onFinish={customOnSubmit}
   >
     
     { propertiesConfig.map( (item: IFormField, index: number) => {
