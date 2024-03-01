5a8d62c026f1c432a44e29933e5002aff1f61c97
Gaurav Singh <gauravsingh.re@gmail.com>
2024-03-01 18:05:55 +0530

    button actions - TBD

---
+++ src/core/forms/ButtonAction/ButtonAction.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+/** A function that should able to call the API from given payload, 
+ *      if there are failures, display the error
+ *      if success, display the success message
+ *      return error messages and success response
+ */
+interface IButtonAction {
+    
+}
+export const buttonAction = () => {
+}
