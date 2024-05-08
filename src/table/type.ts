df826d1935622cfaf8195b2546318ad5a121686b
Gaurav Singh <gauravsingh.re@gmail.com>
2024-05-08 17:15:05 +0530

    type moved to separate file

---
+++ src/table/type.ts
@@ -0,0 +1,33 @@
+import { IApiConfig } from "../core";
+import { IModalConfig } from "../modal/Modal";
+type ITablePagination = "default";
+
+export interface ITableConfig {
+  propertiesConfig: Array<ITablePropertiesConfig>;
+  apiConfig: IApiConfig;
+  records?: Array<any>;
+  paginationType?: ITablePagination;
+}
+
+export interface ITablePropertiesConfig {
+  name: string;
+  dataIndex: string;
+  actions?: Array<IPageAction>;
+  hidden?: boolean;
+  isFilterable?: boolean;
+}
+export type IPageAction = {
+  url: string;
+  label: string;
+  icon: string;
+  htmlType: string;
+  openInModel?: boolean;
+  modelConfig?: IModalConfig;
+};
+
+export interface IActionIndexValue {
+  [key: string]: Array<IPageAction>;
+}
+export interface IRecord {
+  [key: string]: string;
+}
