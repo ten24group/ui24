e723338bbb580d54caaadc7f23d85f6c588e02d0
Gaurav Singh <gauravsingh.re@gmail.com>
2024-05-13 19:39:42 +0530

    separated filter operators list

---
+++ src/table/Filters/filterOperators.ts
@@ -0,0 +1,68 @@
+interface IFilterOperator {
+    label: string;
+    value: string;
+    fieldType?: Array<string>
+}
+
+export const getFilterOperatorByValue = (value: string) => {
+    return filterOperators.find((operator) => operator.value === value)?.label;
+}
+
+export const filterOperators: Array<IFilterOperator> = [{
+    label: "Equals",
+    value: "eq",
+}, {
+  label: "Not Equals",
+    value: "neq",
+}, {
+  label: "Greater Than",
+    value: "gt",
+    fieldType: ["number"]
+}, {
+  label: "Less Than",
+    value: "lt",
+    fieldType: ["number"]
+}, {
+  label: "Greater Than Equals",
+    value: "gte",
+    fieldType: ["number"]
+}, {
+  label: "Less Than Equals",
+    value: "lte",
+    fieldType: ["number"]
+}, {
+  label: "Between",
+    value: "bt",
+}, {
+  label: "Is Empty",
+    value: "isEmpty",
+}, {
+  label: "Is Null",
+    value: "isNull",
+}, {
+  label: "Contains",
+    value: "contains",
+}, {
+  label: "Does Not Contain",
+    value: "notContains",
+}, {
+  label: "Contains Some",
+    value: "containsSome"
+},
+{
+  label: "Starts With",
+    value: "startsWith",
+}, {
+  label: "Ends With",
+    value: "endsWith",
+}, {
+  label: "Like",
+    value: "like",
+}, {
+  label: "In List",
+    value: "in",
+}, {
+  label: "Not In List",
+    value: "nin",
+}
+]
