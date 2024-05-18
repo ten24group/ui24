34f8082cb0d8f3eeaa6eef2b7a306c5d09f24705
Gaurav Singh <gauravsingh.re@gmail.com>
2024-05-18 17:38:05 +0530

    mvoed to root

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-export { UI24 } from './ui24'
 //Post Login pages
 export { PostAuthPage } from "./PostAuth/PostAuthPage"
 export { Header } from '../layout/PostAuthLayout/Header/Header'
