4e286ef133b9985386dc898c0db24aa0b2c89230
Gaurav Singh <gauravsingh.re@gmail.com>
2024-05-18 17:37:02 +0530

    moved to root

---
+++ src/UI24.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { AppRouter, IAppRouter } from './routes/AppRouter';
+
+const UI24 = ({ customRoutes = [] }: IAppRouter ) => {
+    
+    return <AppRouter customRoutes = { customRoutes } />
+}
+export { UI24 };
