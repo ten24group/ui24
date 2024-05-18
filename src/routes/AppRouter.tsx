9908150d439e2962c5f12e96357491cb865b23c9
Gaurav Singh <gauravsingh.re@gmail.com>
2024-05-18 17:39:44 +0530

    fixed routing

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,10 +1,10 @@
 import React, { ReactNode, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import { App as AntdApp } from "antd";
-
+import { useAuth } from '../core/api/config';
+import { PostAuthLayout } from '../layout';
 //import { LoginPage, ForgotPasswordPage, ResetPasswordPage } from "../index";
 import { LoginPage, ForgotPasswordPage, ResetPasswordPage, DynamicPage, PostAuthPage, RegistrationPage, VerifyRegistrationPage } from '../pages';
-import { useAuth } from '../core/api/config';
 
 interface IRoute{
     path: string;
@@ -17,39 +17,40 @@ type IRoutes = Array<IRoute>
 export type IAppRouter = { customRoutes?: IRoutes }
 
 /**
- * AppNavigator component to redirect to another route.
+ * AppNavigator component to redirect the base / route based on auth status.
  * 
  * @param path - The path to navigate to.
  * @returns The empty component.
  */
-const AppNavigator = ({ path } : { path: string }) => {
+export const AppNavigator = () => {
+  const { isLoggedIn } = useAuth();
+
   const navigate = useNavigate();
   useEffect(() => {
-    navigate(path);
-  }, [] )
+    navigate( isLoggedIn ? '/dashboard' : '/login');
+  }, [ isLoggedIn ] )
   return <></>
 }
 
 export const AppRouter = ({ customRoutes = [] } : IAppRouter ) => {
-
-    const auth = useAuth();
 
     // Default routes
     const defaultRoutes: IRoutes = [
       { path: "/login", element: <LoginPage /> },
       { path: "/registration", element: <RegistrationPage /> },
       { path: "/verification", element: <VerifyRegistrationPage /> },
-      { path: "/forgot-password", element: <ForgotPasswordPage /> },
+      { path: "/forgot-password", element: <ForgotPasswordPage />},
       { path: "/reset-password", element: <ResetPasswordPage /> },
-      { path: "/dashboard", element: <PostAuthPage  metaDataUrl='Dashboard' pageTitle="Dashboard" pageType='dashboard'/> },
-      { path: "/", element: <AppNavigator path= { !auth.isLoggedIn() ? "/login" : '/dashboard' } /> },
+      { path: "/", element: <AppNavigator /> },
       { path: "/:dynamicPage", element : <DynamicPage /> },
       { path: "/:dynamicPage/:dynamicID", element : <DynamicPage /> },
     ];
   
     // Merge custom routes with default routes, giving precedence to custom ones
-    const mergedRoutes = [...defaultRoutes, ...customRoutes].reduce((acc: any, route: IRoute, index:number ) => {
-      acc[route.path] = <React.Fragment key={"route"+index}><Route path={route.path} element={route.element} /></React.Fragment>;
+    const mergedRoutes = [...customRoutes, ...defaultRoutes ].reduce((acc: any, route: IRoute, index:number ) => {
+      acc[route.path] = <React.Fragment key={"route"+index}>{}
+        <Route path={route.path} element={route.element} />
+      </React.Fragment>;
       return acc;
     }, {});
   
