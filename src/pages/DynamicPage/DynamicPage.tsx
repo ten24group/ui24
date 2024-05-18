b0f9339929111ab86bd5772b49932129fb86d2b0
Gaurav Singh <gauravsingh.re@gmail.com>
2024-05-18 17:38:41 +0530

    dynamic page usable for both public and private pages

--- src/pages/DynamicPage/DynamicPage.tsx
+++ src/pages/DynamicPage/DynamicPage.tsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import { PostAuthPage } from '../PostAuth/PostAuthPage';
+import { PreAuthLayout } from '../../layout';
 import { useParams } from "react-router-dom"
-
+import { useAuth } from '../../core';
 import { UI24Config } from '../../core';
+import { AppNavigator } from '../../routes/AppRouter';
+import { NotFound } from '../404/NotFound';
 
 export const DynamicPage = () => {
     //get routes from URL
     const { dynamicPage = "", dynamicID = ""} = useParams()
+    
+    const { isLoggedIn } = useAuth();
 
     let pageConfig = UI24Config.uiConfig.pages[dynamicPage]
     //check if page config exists for the route
     if( !pageConfig ){
-        //Fallback : make API call to get page config
+        //TODO: Fallback : make API call to get page config
     }
 
     if( !pageConfig ){
-        return <h3>404</h3>
+        return <NotFound />
     }
 
-    return <PostAuthPage {...pageConfig} />;
+    if( !isLoggedIn && pageConfig?.private === true ){
+        return <AppNavigator />
+    }
+
+    return ( pageConfig?.private === true || ( pageConfig?.private ?? true ) ) ? <PostAuthPage {...pageConfig} /> : <PreAuthLayout>
+        <h3>Define your page.</h3>
+    </PreAuthLayout>;
 }
