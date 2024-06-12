3346dd95bdab08c6e1c4bc48cfc0aab5be00929f
Gaurav Singh <gauravsingh.re@gmail.com>
2024-06-12 13:47:06 +0530

    name change

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,6 @@ export * from "./pages";
 export * from "./layout";
 export { AppRouter } from './routes/AppRouter'
 export * from "./core";
-export * from "./forms/PostAuthForm";
+export * from "./forms/Form";
 export * from "./table/Table";
 export * from "./detail/Details";
