bc085f0d9c299f8218165bb4f684c29891681c02
Gaurav Singh <gauravsingh.re@gmail.com>
2024-06-12 13:56:18 +0530

    name change port

--- src/pages/PostAuth/PostAuthPage.tsx
+++ src/pages/PostAuth/PostAuthPage.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { PostAuthLayout } from "../../layout"
 import { PageHeader, IPageHeader } from './PageHeader/PageHeader';
-import { ICustomForm } from '../../core/forms/formConfig';
+import { IForm } from '../../core/forms/formConfig';
 import "./PostAuthPage.css";
 import { Card } from 'antd';
-import { PostAuthForm } from '../../forms/PostAuthForm';
+import { Form } from '../../forms/Form';
 import { Table } from '../../table/Table';
 import { ITableConfig } from '../../table/type';
 import { Details, IDetailsConfig } from '../../detail/Details';
@@ -15,9 +14,10 @@ type IPageType = "list" | "form" | "accordion" | "details" | "dashboard";
 interface IRenderFromPageType {
     pageType?: IPageType;
     cardStyle?: React.CSSProperties;
-    formPageConfig?: ICustomForm;
+    formPageConfig?: IForm;
     listPageConfig?: ITableConfig;
     detailsPageConfig?: IDetailsConfig;
+    //accordion?: Record<string, <ICustomForm | ITableConfig | IDetailsConfig>>
 }
 
 export interface IPostAuthPage extends IPageHeader, IRenderFromPageType {
@@ -27,8 +27,7 @@ export interface IPostAuthPage extends IPageHeader, IRenderFromPageType {
 
 
 export const PostAuthPage = ({ CustomPageHeader, children, ...props } : IPostAuthPage ) => {
-    return <PostAuthLayout>
-        <div style={{ paddingTop: "1%"}}>
+    return <div style={{ paddingTop: "1%"}}>
             <div className = "PostAuthContainer" >
                 { CustomPageHeader ? CustomPageHeader : <PageHeader {...props} />}
                 <div className="PageContent">
@@ -37,15 +36,14 @@ export const PostAuthPage = ({ CustomPageHeader, children, ...props } : IPostAut
                 </div>
             </div>
         </div>
-    </PostAuthLayout>;
 }
 
 export const RenderFromPageType = ( {pageType, cardStyle, formPageConfig, listPageConfig, detailsPageConfig}: IRenderFromPageType ) => {
     
     switch( pageType ) {
         case "list": return <Card style={ cardStyle } > <Table {...listPageConfig} key={`list-${uuidv4()}`} /> </Card>;
-        case "form": return <Card style={ cardStyle } > <PostAuthForm {...formPageConfig} /> </Card>;
-        case "details": return <Card style={ cardStyle } > <Details {...detailsPageConfig} /> </Card>;
+        case "form": return <Card style={ cardStyle } > <Form {...formPageConfig} key={`form-${uuidv4()}`} /> </Card>;
+        case "details": return <Card style={ cardStyle } > <Details {...detailsPageConfig}  key={`details-${uuidv4()}`}/> </Card>;
         case "accordion": return <div> Accordion Page </div>;
         case "dashboard": return <Card style={ cardStyle } >  </Card>;
         default: return <>Invalid Page Type</>;
