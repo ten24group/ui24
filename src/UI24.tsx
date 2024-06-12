8ce00478f9d1574f3a0172b1bedaa5e9d061e9f6
Gaurav Singh <gauravsingh.re@gmail.com>
2024-06-12 13:47:37 +0530

    modified UI24 component to accept config

--- src/UI24.tsx
+++ src/UI24.tsx
@@ -1,8 +1,51 @@
 import React from 'react';
 import { AppRouter, IAppRouter } from './routes/AppRouter';
+import { Ui24ConfigProvider, AuthProvider, ApiProvider, IApiConfig } from './core/context';
 
-const UI24 = ({ customRoutes = [] }: IAppRouter ) => {
+type ConfigResolver<T extends unknown> = T // the config itself
+| string  // config url/endpoint
+| ( () => Promise<T> ) // a function that resolves the config
+
+type IUI24 = {
+    customRoutes?: IAppRouter["customRoutes"];
+    ui24config: {
+        baseURL: string;
+        appLogo: string;
+        uiConfig: {
+            auth: ConfigResolver<any>,
+            menu: ConfigResolver<any>,
+            pages: ConfigResolver<any>;
+            dashboard: ConfigResolver<any>;
+        }
+        appName: string;
+        layouts: {
+            authLayout: React.ReactNode;
+            publicLayout: React.ReactNode;
+            privateLayout: React.ReactNode;
+        }
+        auth?: {
+            verifyToken: IApiConfig
+            redirectUrl?: string
+        }
+        routes: Array<{
+            route: string;
+            section: React.ReactNode;
+            authType: "auth" | "public" | "private";
+        }>
+        authProvider: string;
+        //customerInfo: any;
+        apiConfig: any;
+    }
+}
+
+const UI24 = ({ customRoutes = [], ui24config }: IUI24 ) => {
     
-    return <AppRouter customRoutes = { customRoutes } />
+    return <Ui24ConfigProvider initConfig={ ui24config }>
+        <AuthProvider>
+            <ApiProvider>
+                <AppRouter customRoutes = { customRoutes } />
+            </ApiProvider>
+        </AuthProvider>
+    </Ui24ConfigProvider>
 }
 export { UI24 };
