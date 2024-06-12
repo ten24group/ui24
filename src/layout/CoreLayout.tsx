75d3c3f2869c2d6c513e9dfeb4743288524239fd
Gaurav Singh <gauravsingh.re@gmail.com>
2024-06-12 13:53:34 +0530

    added dynamic layout based on route type

--- src/layout/CoreLayout.tsx
+++ src/layout/CoreLayout.tsx
@@ -1,12 +1,39 @@
 import React, { ReactNode } from 'react';
 import { Layout } from 'antd';
 import { AppContextProvider } from '../core/context/AppContext';
+import { AuthLayout } from './AuthLayout/AuthLayout';
+import { PublicLayout } from './PublicLayout/PublicLayout';
+import { PrivateLayout } from './PrivateLayout/PrivateLayout';
+import { IRouteAuthType } from '../routes/types';
+import { useUi24Config } from '../core/context';
 
-export const CoreLayout = ({children} : { children? : ReactNode } ) => {
+export const CoreLayout = ({children, authType = "public"} : { children? : ReactNode, authType?: IRouteAuthType } ) => {
+    const { selectConfig } = useUi24Config()
+    const layouts = selectConfig( (config) => config.layouts )
     
+    
+    // Determine which layout to use based on route type
+    const getLayoutComponent = (
+        type: IRouteAuthType
+    ) => {
+        switch (type) {
+            case "auth":
+                return layouts?.authLayout || AuthLayout;
+            case "public":
+                return layouts?.publicLayout || PublicLayout;
+            case "private":
+                return layouts?.privateLayout || PrivateLayout;
+            default:
+                return AuthLayout;
+        }
+    };
+
+  const WrapperLayout = getLayoutComponent(authType);
     return <Layout style={{ minHeight: '100vh' }}>
         <AppContextProvider>
-            {children}
+            <WrapperLayout>
+                {children}
+            </WrapperLayout>
         </AppContextProvider>
     </Layout>
 }
