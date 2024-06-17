d2f202d2f847ee12e9593d0ceda0e18b9ab4f3e5
Gaurav Singh <gauravsingh.re@gmail.com>
2024-06-17 21:27:10 +0530

    config resolver made dynamic based on type

--- src/common/FullPagesConfigLoader.tsx
+++ src/common/FullPagesConfigLoader.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, useRef, useState, useEffect, ReactNode } from 'react';
-import { useApi, useAuth, useUi24Config } from '../core/context';
+import { useAuth, useUi24Config } from '../core/context';
 import { Spin } from 'antd';
+import { IConfigResolver } from '../core/context';
 
 const FullPagesConfigLoaderContext = createContext<undefined>(undefined);
 
@@ -8,19 +9,24 @@ export const FullPagesConfigLoader: React.FC<{ children: ReactNode }> = ({ child
     const { selectConfig, updateConfig }  = useUi24Config()
     const [ loader, setLoader ] = useState(false)
     const { isLoggedIn } = useAuth()
-    const { callApiMethod } = useApi()
 
     const pagesConfig = selectConfig( ( config ) => config.pagesConfig || [] )
 
     const { pages: pageConfigUrl, menu : menuConfigUrl, dashboard } = selectConfig( (config) => config.uiConfig )
     const initPageConfig = useRef(false)
 
-    const loadConfigsFromUrls = async <T extends any[]>(...urls: string[]): Promise<T>  => {
+    const loadConfigs = async <T extends IConfigResolver<any>[]>(...props: IConfigResolver<any>[]): Promise<T>  => {
         const configs = await Promise.all( 
-            urls.map( async (url) => { 
-                const config = await fetch(url);
-                const resolved = await config.json();
-                return resolved;
+            props.map( async (prop) => { 
+                if( typeof prop === 'function' ) {
+                    return await prop()
+                } else if( typeof prop === 'string' ) {
+                    const config = await fetch(prop);
+                    const resolved = await config.json();
+                    return resolved;
+                } else if( typeof prop === 'object' ) {
+                    return prop
+                }
             }) 
         );
         return configs as T;
@@ -31,7 +37,7 @@ export const FullPagesConfigLoader: React.FC<{ children: ReactNode }> = ({ child
         async function loadPagesConfig() {
             setLoader( true )
 
-            const resolver  = await loadConfigsFromUrls(pageConfigUrl, menuConfigUrl, dashboard)
+            const resolver  = await loadConfigs(pageConfigUrl, menuConfigUrl, dashboard)
             const { [0] : response, [1] : menuResponse, [2]: dashboardResponse } = resolver
 
             const configPayload = {
