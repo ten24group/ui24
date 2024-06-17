4dd5bfe52b82a52342d9e1c01b4c58a1e6772f7a
Gaurav Singh <gauravsingh.re@gmail.com>
2024-06-17 21:27:58 +0530

    simplified, link can be used a dynamic sub component of <a/>

--- src/core/common/Link/Link.tsx
+++ src/core/common/Link/Link.tsx
@@ -1,14 +1,31 @@
 import { useNavigate } from 'react-router-dom';
 import React, { Component, ReactNode } from 'react';
 
-type ILinkProps = { url?: string; title?: string; children?: ReactNode, onClick?: Function } | { url: string; children: ReactNode; title?: string, onClick?: Function };
+type ICommonLinkProps = {
+  title?: string;
+  children?: ReactNode
+} & React.AnchorHTMLAttributes<HTMLAnchorElement>
 
-export function Link ({title, url, children, onClick }: ILinkProps ) {
+type ILinkProps = ({ url?: string; onClick: (url?: string) => void } | { url: string; onClick?: (url?: string) => void }) & ICommonLinkProps;
+
+export function Link ({title, url, children, onClick, ...props }: ILinkProps ) {
   const navigate = useNavigate();
-  return <a onClick={ () => {
-    onClick && onClick(url)
-    !onClick && navigate(url)
-    } } className="forgotPassowrd">
+
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault(); // Prevent the default anchor behavior
+    //console.log("triggered")
+    if( onClick ) {
+      onClick(url)
+    }
+
+    if( url ) {
+      navigate(url);
+    }
+  };
+
+  //console.log("Link", url, onClick)
+
+  return <a onClick={ handleClick }  {...props} >
       { title !== "" ? title : "" } { children !== undefined ? children : "" }
     </a>
 }
