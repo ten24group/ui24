315c20b8a7852d14b3593ebdff49c07622d6d409
Nitin Yadav <nitinrajyadav@outlook.com>
2024-06-17 18:17:23 +0530

    Fix: block-note editor crashing when using menu

--- src/core/common/CustomBlockNoteEditor.tsx
+++ src/core/common/CustomBlockNoteEditor.tsx
@@ -2,23 +2,23 @@ import React, { useEffect, useMemo, useState } from "react";
 
 import { useCreateBlockNote } from "@blocknote/react";
 import { BlockNoteView } from "@blocknote/mantine";
-
+import { Block, BlockNoteEditorOptions } from "@blocknote/core";
 import "@blocknote/core/fonts/inter.css";
 import "@blocknote/mantine/style.css";
-import { Block, BlockNoteEditor, BlockNoteEditorOptions } from "@blocknote/core";
+
 import { GetSignedUploadUrlAPIConfig, S3FileUploaderSuccessResponse, useS3FileUploader } from "./FileUploader";
 
 export type BlockNoteEditorProps = 
-Partial<Omit<BlockNoteEditorOptions<any, any, any>, 'initialContent' | 'onChange'>> 
-& Omit<React.ComponentProps<typeof BlockNoteView>, 'editor'>
-& {
-  value?: Block[],
-  onChange?: (data?: Block[]) => void,
-  readOnly?: boolean,
+  Partial<Omit<BlockNoteEditorOptions<any, any, any>, 'initialContent' | 'onChange'>> 
+  & Omit<React.ComponentProps<typeof BlockNoteView>, 'editor'>
+  & {
+    value?: Block[],
+    onChange?: (data?: Block[]) => void,
+    readOnly?: boolean,
 
-  fileNamePrefix?: string,
-  getSignedUploadUrlAPIConfig?: GetSignedUploadUrlAPIConfig,
-};
+    fileNamePrefix?: string,
+    getSignedUploadUrlAPIConfig?: GetSignedUploadUrlAPIConfig,
+  };
  
 export const CustomBlockNoteEditor: React.FC<BlockNoteEditorProps> = ({ 
   value,
@@ -33,6 +33,8 @@ export const CustomBlockNoteEditor: React.FC<BlockNoteEditorProps> = ({
 
   ...restProps
 }) => {
+
+  value = value || [{ type: 'paragraph', id: '__default_block___'} as any];
   
   const [blocks, setBlocks] = useState<Block[]>(value);
   const [htmlContent, setHtmlContent] = useState<string>('');
@@ -64,47 +66,21 @@ export const CustomBlockNoteEditor: React.FC<BlockNoteEditorProps> = ({
     return Promise.resolve({props: response.data});
   };
 
-  // Creates a new editor instance.
-  // We use useMemo + createBlockNoteEditor instead of useCreateBlockNote so we
-  // can delay the creation of the editor until the initial content is loaded.
-  const editor = useMemo(() => {
-    
-    let editorInst: any;
-    
-    if(value && Array.isArray(value) && value.length > 0) {
-      editorInst = BlockNoteEditor.create({ initialContent: value, uploadFile: customFileUploader || defaultFileUploader });
-    } else {
-      editorInst = BlockNoteEditor.create({ uploadFile: customFileUploader || defaultFileUploader });
-    }
-
+  const editor = useCreateBlockNote({initialContent: value, uploadFile: customFileUploader || defaultFileUploader });
 
   if(readOnly){
-    editorInst.blocksToHTMLLossy(value).then((html) => {
+    editor.blocksToHTMLLossy(value).then((html) => {
       setHtmlContent(html);
     })
   }
 
-    return editorInst;
-
-  }, [value]);
- 
-  if (editor === undefined) {
-    return "Loading contents...";
-  }
-
-  // const editor = useCreateBlockNote({ uploadFile: customFileUploader || defaultFileUploader });
-
   const onChange = () => {
     console.log("Editor doc has been changed:", editor.document, editor.isEditable);
     setBlocks(editor.document);
+    // notify the parent component when the editor content changes.
+    customOnChange && customOnChange(blocks);
   };
   
-  // notify the parent component when the editor content changes.
-  const onBlur = () => {
-    // notify the form about the changed content
-    customOnChange && customOnChange(blocks);
-  }
-
   // Renders the editor instance using a React component.
   return <div className={"item bordered"}>
       { readOnly && 
@@ -119,7 +95,6 @@ export const CustomBlockNoteEditor: React.FC<BlockNoteEditorProps> = ({
           {...restProps} 
           theme={theme}
           editor={editor} 
-          onBlur={onBlur}
           onChange={onChange} 
         /> 
       }
