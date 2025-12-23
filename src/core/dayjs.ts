87ec88487b3dc1ead5c0e512033bb80cf888676c
Gaurav Singh <gauravsingh.re@gmail.com>
2025-12-23 17:30:22 +0530

    fix for calendar plugin referring local data plugin

--- src/core/dayjs.ts
+++ src/core/dayjs.ts
@@ -4,11 +4,13 @@ import dayjsUTCPlugin from 'dayjs/plugin/utc';
 import dayjsTimezonePlugin from 'dayjs/plugin/timezone';
 import dayjsCustomParseFormat from 'dayjs/plugin/customParseFormat';
 import dayjsWeekdayPlugin from 'dayjs/plugin/weekday';
+import dayjsLocaleDataPlugin from 'dayjs/plugin/localeData';
 
 dayjs.extend(dayjsUTCPlugin);
 dayjs.extend(dayjsTimezonePlugin);
 dayjs.extend(dayjsCustomParseFormat);
 dayjs.extend(dayjsWeekdayPlugin);
+dayjs.extend(dayjsLocaleDataPlugin);
 
 dayjs.tz.setDefault('UTC');
 
